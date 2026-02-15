ae90d6dc0d0f4049e39ad8a63645c93bf66a9ba8
Joern Barthel <joern@barthel.digital>
2026-02-15 22:21:44 +0100

    feat: add range iterator for order

--- src/content/order.rs
+++ src/content/order.rs
@@ -1,10 +1,25 @@
+use std::iter::Rev;
+use std::ops::Range;
+
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Order {
     Asc,
     Desc,
 }
 
+pub enum OrderedRange {
+    Asc(Range<usize>),
+    Desc(Rev<Range<usize>>),
+}
+
 impl Order {
+    pub fn range(self, r: Range<usize>) -> OrderedRange {
+        match self {
+            Order::Asc => OrderedRange::Asc(r),
+            Order::Desc => OrderedRange::Desc(r.rev()),
+        }
+    }
+
     pub fn sort_unstable_by_key<T, K: Ord>(&self, slice: &mut [T], mut f: impl FnMut(&T) -> K) {
         match self {
             Order::Asc => slice.sort_unstable_by(|a, b| f(a).cmp(&f(b))),
@@ -13,10 +28,41 @@ impl Order {
     }
 }
 
+impl Iterator for OrderedRange {
+    type Item = usize;
+
+    #[inline]
+    fn next(&mut self) -> Option<usize> {
+        match self {
+            OrderedRange::Asc(r) => r.next(),
+            OrderedRange::Desc(r) => r.next(),
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        match self {
+            OrderedRange::Asc(r) => r.size_hint(),
+            OrderedRange::Desc(r) => r.size_hint(),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use proptest::prelude::*;
+
+    #[test]
+    fn range_asc() {
+        let v: Vec<usize> = Order::Asc.range(2..5).collect();
+        assert_eq!(v, vec![2, 3, 4]);
+    }
+
+    #[test]
+    fn range_desc() {
+        let v: Vec<usize> = Order::Desc.range(2..5).collect();
+        assert_eq!(v, vec![4, 3, 2]);
+    }
 
     proptest! {
         #[test]
